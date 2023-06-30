60af566580172827166aebc49682b3fb2a7f04ba
Caio Vinicius <85201218+caiovinn@users.noreply.github.com>
2023-06-29 21:56:28 -0300

    atualizacao de medidas

--- CONSTANTES.py
+++ CONSTANTES.py
@@ -3,8 +3,29 @@
 ########################################################################
 import networkx as nx
 import numpy as np
+import math
 
-# definicao da semente, em prol da reprodutibilidade:
+
+def degree_distribution(GER):
+    vk = dict(GER.degree())
+    vk = list(vk.values()) # we get only the degree values
+    maxk = np.max(vk)
+    mink = np.min(min)
+    kvalues= np.arange(0,maxk+1) # possible values of k
+    Pk = np.zeros(maxk+1) # P(k)
+    for k in vk:
+        Pk[k] = Pk[k] + 1
+    Pk = Pk/sum(Pk) # the sum of the elements of P(k) must to be equal to one
+    return kvalues,Pk
+
+
+def shannon_entropy(G):
+    k,Pk = degree_distribution(G)
+    H = 0
+    for p in Pk:
+        if(p > 0):
+            H = H - p*math.log(p, 2)
+    return H
 
 # quantidade de vezes que deve simular os modelos:
 VEZES = 10
@@ -20,7 +41,8 @@ redes_sociais = {'twitter-pequeno': 'ego-twitter/out.ego-twitter'
 # de forma que: elas recebam (N, k) e retornem o grafo.
 erdos    = lambda N, k: nx.erdos_renyi_graph(N, k/(N-1))
 barabasi = lambda N, k: nx.barabasi_albert_graph(N, int(k/2))
-wattz =    lambda N, k: nx.watts_strogatz_graph(N, int(k/2),1)
+wattz =    lambda N, k: nx.watts_strogatz_graph(N, int(k),p = 1) ####Debugar
+
 
 
 # dicionario com as funcoes para simular os modelos
@@ -37,7 +59,11 @@ grau_medio = lambda G: np.array(G.degree)[:, 1].mean()
 # dicionario com as funcoes para calcular as medidas
 MEDIDAS = {'grau_medio': grau_medio,
            'assortatividade':nx.degree_assortativity_coefficient,
-           'coef_clusterizacao': nx.average_clustering
+           'coef_clusterizacao': nx.average_clustering,
+           'transitivade' : nx.transitivity,
+           'entropy' : shannon_entropy
+          # 'average_shortests_path_lenght' : nx.average_shortest_path_length
+
            #'diametro': nx.diameter
            #'menor_caminho':nx.betweenness_centrality
         }
